7654a706e42065d779d5c9b6d30249fdc80489dc
MasoudKarimi04 <74917502+MasoudKarimi04@users.noreply.github.com>
2021-01-07 11:06:35 -0500

    Update pipe.js

--- pipe.js
+++ pipe.js
@@ -3,7 +3,11 @@
 // http://patreon.com/codingtrain
 // Code for: https://youtu.be/cXgA1d_E-jY&
 
+// Changed and edited by Masoud and Salim
+
 function Pipe() {
+  
+  // Pipe aspects / function
   this.spacing = 175;
   this.top = random(height / 6, 3 / 4 * height);
   this.bottom = height - (this.top + this.spacing);
@@ -13,6 +17,7 @@ function Pipe() {
 
   this.highlight = true;
 
+  // Checking if the bird gets within the pipe's dimensions
   this.hits = function(bird) {
     if (bird.y < this.top || bird.y > height - this.bottom) {
       if (bird.x > this.x && bird.x < this.x + this.w) {
@@ -24,8 +29,9 @@ function Pipe() {
     return false;
   }
 
+  // Showing the pipe
   this.show = function() {
-    fill(255);
+        fill(0, 55, 0);
     if (this.highlight) {
       fill(255, 0, 0);
     }
@@ -33,6 +39,7 @@ function Pipe() {
     rect(this.x, height - this.bottom, this.w, this.bottom);
   }
 
+  // Speed 
   this.update = function() {
     this.x -= this.speed;
   }
